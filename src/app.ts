161353269703e31d0f51a7a8dd58afd16efcde29
Erik Lee <2141800+aethertap@users.noreply.github.com>
2025-02-15 16:13:59 -0600

    got it working

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,7 @@
 
 import {Common,Events,Engine,Render,Runner,Bodies,Composite,Body,Vector} from "matter-js";
 import Tank from "./tank";
-
-// module aliases
-//var Engine = Matter.Engine,
-//    Render = Matter.Render,
-//    Runner = Matter.Runner,
-//    Bodies = Matter.Bodies,
-//    Composite = Matter.Composite;
-
-function thrust_update(tank:Body, _dt:number) {
-  let pointing = Vector.create(Math.cos(tank.angle),Math.sin(tank.angle));
-  let pos = tank.position;
-  let angle = tank.angle;
-  Body.applyForce(tank,
-    Vector.add(pos, Vector.rotate(Vector.create(0,40),angle)),
-    Vector.mult(pointing,0.06));
-  Body.applyForce(tank,
-    Vector.add(pos, Vector.rotate(Vector.create(0,-40),angle)),
-    Vector.mult(pointing,0.006));
-  Body.setVelocity(tank, Vector.mult(pointing,Vector.dot(tank.velocity, pointing)));
-  if(Vector.magnitude(tank.velocity) > 20) {
-    let shrink = 20/Vector.magnitude(tank.velocity);
-    Body.setVelocity(tank,Vector.mult(tank.velocity,shrink));
-    Body.setAngularSpeed(tank,tank.angularSpeed*shrink);
-  }
-}
-
-// create an engine
-var engine = Engine.create();
-engine.gravity.scale=0;
-
-// create a renderer
-var render = Render.create({
-  element: document.querySelector('#game'),
-  engine: engine
-});
+import { Game } from "./game";
 
 let tank_code = 
   `
@@ -59,52 +25,13 @@ let tank_code =
   }
   //console.log("Inside tank code: controls = ",controls);
   controls.turn_gun = 1.0;
-  //drive_speed_and_angle(controls,20.0,1);
-  drive_radius(controls, 60, 60);
+  drive_radius(controls, 60, 1000);
   return controls;
 `;
 
 // create two boxes and a ground
 var tank = new Tank(60, 100, tank_code, Vector.create(200,200));
-var circ = Bodies.circle(200,290,59);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-Body.setMass(tank.body,100.0);
-
-// add all of the bodies to the world
-Composite.add(engine.world, [tank.body, circ, ground]);
-
-// run the renderer
-Render.run(render);
-
-// create runner
-var runner = Runner.create();
-runner.delta = 1/360.0;
-
-Events.on(engine, 'beforeUpdate', function(event) {
-  var engine = event.source;
-  tank.control(engine.timing.lastDelta);
-  tank.update(engine.timing.lastDelta);
-  //thrust_update(tank,event.source.timing.lastDelta);
-  //console.log(`Velocity is ${JSON.stringify(tank.velocity)}, location is ${JSON.stringify(tank.position)}`);
-});
-
-let dt = 0.016;
-function millis() {
-  let d = new Date();
-  return d.getTime()/1000.0;
-}
-let last_time = millis();
-
-function frame(t:number) {
-  // run the engine
-  requestAnimationFrame((t:number) => frame(t));
-  let this_time = millis();
-  dt = this_time-last_time;
-  last_time = this_time;
-  Engine.update(engine,dt);
-}
-requestAnimationFrame(frame);
-//Runner.run(runner, engine);
-
-
+let game = new Game();
+game.add_tank(tank);
+game.run();
--- src/game.ts
+++ src/game.ts
@@ -19,6 +19,7 @@ export class Game {
     this.engine.gravity.scale = 0;
     this.tanks = [];
     this.animation_id = undefined;
+    this.last_update=-1;
     // create a renderer
     this.render = Render.create({
       element: document.querySelector('#game'),
@@ -27,11 +28,12 @@ export class Game {
     if(!this.render) {
       throw new Error("Couldn't build a renderer!");
     }
+    
     Composite.add(this.engine.world,[
-      Bodies.rectangle(0,0,1,Game.bounds.height),
-      Bodies.rectangle(0,0,Game.bounds.width, 1),
-      Bodies.rectangle(0,Game.bounds.height, Game.bounds.width, 1),
-      Bodies.rectangle(Game.bounds.width, 0, 1,Game.bounds.height),
+      Bodies.rectangle(0,Game.bounds.height/2,1,Game.bounds.height,{isStatic:true}),
+      Bodies.rectangle(Game.bounds.width/2,0,Game.bounds.width, 1,{isStatic:true}),
+      Bodies.rectangle(Game.bounds.width/2,Game.bounds.height, Game.bounds.width, 1,{isStatic:true}),
+      Bodies.rectangle(Game.bounds.width, Game.bounds.height/2, 1,Game.bounds.height,{isStatic:true}),
     ]);
     this.register_updates();
   }
@@ -46,7 +48,13 @@ export class Game {
     })
   }
 
+  add_tank(tank:Tank) {
+    this.tanks.push(tank);
+    Composite.add(this.engine.world,[tank.body]);
+  }
+  
   run() {
+    Render.run(this.render);
     this.animation_id = requestAnimationFrame((_:number)=>this.update())  
   }
   
@@ -56,69 +64,13 @@ export class Game {
       this.last_update = this_update-1/60.;
     }
     let delta_t = this_update-this.last_update;
-    this.last_update = new Date().getTime()/1000.0;
     if(delta_t >= 1/max_fr) {
       Engine.update(this.engine,delta_t);
+      this.last_update = new Date().getTime()/1000.0;
     }
     this.animation_id = requestAnimationFrame(()=>this.update(max_fr))
   }
 }
 
 
-/*
-import {Common,Events,Engine,Render,Runner,Bodies,Composite,Body,Vector} from "matter-js";
-import Tank from "./tank";
 
-// module aliases
-//var Engine = Matter.Engine,
-//    Render = Matter.Render,
-//    Runner = Matter.Runner,
-//    Bodies = Matter.Bodies,
-//    Composite = Matter.Composite;
-
-engine.gravity.scale=0;
-
-// create two boxes and a ground
-var tank = new Tank(60, 100, tank_code, Vector.create(200,200));
-var circ = Bodies.circle(200,290,59);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-Body.setMass(tank.body,100.0);
-
-// add all of the bodies to the world
-Composite.add(engine.world, [tank.body, circ, ground]);
-
-// run the renderer
-Render.run(render);
-
-// create runner
-var runner = Runner.create();
-runner.delta = 1/360.0;
-
-Events.on(engine, 'beforeUpdate', function(event) {
-  var engine = event.source;
-  tank.control(engine.timing.lastDelta);
-  tank.update(engine.timing.lastDelta);
-  //thrust_update(tank,event.source.timing.lastDelta);
-  //console.log(`Velocity is ${JSON.stringify(tank.velocity)}, location is ${JSON.stringify(tank.position)}`);
-});
-
-let dt = 0.016;
-function millis() {
-  let d = new Date();
-  return d.getTime()/1000.0;
-}
-let last_time = millis();
-
-function frame(t:number) {
-  // run the engine
-  requestAnimationFrame((t:number) => frame(t));
-  let this_time = millis();
-  dt = this_time-last_time;
-  last_time = this_time;
-  Engine.update(engine,dt);
-}
-requestAnimationFrame(frame);
-//Runner.run(runner, engine);
-
-*/
